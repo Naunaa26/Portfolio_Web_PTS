16154d64d5e965ec960e0eefc76656fddcec0ea8
mnt26 <bluenaufal7@gmail.com>
2023-09-20 21:06:34 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+var typed = new Typed(".text", {
+    strings:["Frontend Developer", "Web Designer", "CyberSecurity" ,"Web Developer",],
+    typeSpeed:100,
+    backSpeed:10,
+    backDelay:500,
+    loop: true
+});
+
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 / dots;
+
+
+    for(let i = 0 ; i < dots ; i++){
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for(let i = 0; i<percent ; i++){
+        pointsMarked[i].classList.add('marked')
+    }
+})
+
+var mixer = mixitup('.portofolio-galery');
